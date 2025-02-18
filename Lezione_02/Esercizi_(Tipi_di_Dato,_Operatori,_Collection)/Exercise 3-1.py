43f1e6cc33996fba658bfb54dcce2581b515ce55
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-18 17:15:32 +0100

    Done exercise 3-2

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-1.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-1.py
@@ -1,6 +1,6 @@
 '''Exercise 3-1
 Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.'''
 
-names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia"]
+names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
 for i in names:
     print(i)
--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
@@ -1,3 +1,7 @@
 '''Exercise 3-2
 Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name,
 print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.'''
+
+names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
+for i in names:
+    print(f"Hi {i}! How are you doing today?")
