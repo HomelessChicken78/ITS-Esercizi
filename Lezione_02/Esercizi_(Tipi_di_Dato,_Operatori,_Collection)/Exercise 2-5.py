2b5cf84b7b4c2ae1471f14c1e95aa28b8b1d2a76
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 18:52:29 +0100

    Variable type declaration

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 2-5.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 2-5.py
@@ -3,5 +3,5 @@ Famous Quote: Find a quote from a famous person you admire. Print the quote and 
 Your output should look something like the following, including the quotation marks:
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”'''
 
-message = "Don't believe everything you read on the internet just because there's a picture with a quote next to it."
+message: str = "Don't believe everything you read on the internet just because there's a picture with a quote next to it."
 print(f'''Abraham Lincoln once didn't say: "{message}"''')
