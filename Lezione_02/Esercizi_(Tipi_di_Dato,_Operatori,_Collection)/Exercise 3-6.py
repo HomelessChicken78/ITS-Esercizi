a885b83a4bcd2926f95526365090e9176d378423
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 17:24:26 +0100

    Added 2 more exercises completed

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-6.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-6.py
@@ -5,3 +5,22 @@ More Guests: You just found a bigger dinner table, so now more space is availabl
 • Use insert() to add one new guest to the middle of your list.
 • Use append() to add one new guest to the end of your list.
 • Print a new set of invitation messages, one for each person in your list.'''
+
+guests: list = ["Albert Einstein", "Melon Musk", "Steve Jobs", "Pingproxy", "Quandale Dingle", "Goofy Goof", "Tizio Caio"]
+for i in guests:
+    print(f"Hey {i} wanna come to my party?")
+
+print(f"\n{guests[5]} couldn't make it to the party! :(\n")
+guests.remove("Goofy Goof")
+
+guests.append("Mickey Mouse")
+for i in guests:
+    print(f"Hello {i}, would you like to come to my party?")
+
+print("\nGUYS I FOUND A BIGGER TABLE!!!\n")
+guests.insert(0, "Leandro Pazienza")
+guests.insert(len(guests)//2, "Pikachu")
+guests.append("Socrate")
+
+for i in guests:
+    print(f"Hi {i}. You are invited to my party!")
--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-7.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-7.py
@@ -6,3 +6,35 @@ print a message to that person letting them know you’re sorry you can’t invi
 • Print a message to each of the two people still on your list, letting them know they’re still invited.
 • Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the
 end of your program.'''
+
+guests: list = ["Albert Einstein", "Melon Musk", "Steve Jobs", "Pingproxy", "Quandale Dingle", "Goofy Goof", "Tizio Caio"]
+for i in guests:
+    print(f"Hey {i} wanna come to my party?")
+
+print(f"\n{guests[5]} couldn't make it to the party! :(\n")
+guests.remove("Goofy Goof")
+
+guests.append("Mickey Mouse")
+for i in guests:
+    print(f"Hello {i}, would you like to come to my party?")
+
+print("\nGUYS I FOUND A BIGGER TABLE!!!\n")
+guests.insert(0, "Leandro Pazienza")
+guests.insert(len(guests)//2, "Pikachu")
+guests.append("Socrate")
+
+for i in guests:
+    print(f"Hi {i}. You are invited to my party!")
+
+print("\nMy table won't arrive in time guys! I only got space for two guests :(")
+while (len(guests) > 2):
+    print(f"I am sorry {guests[len(guests) - 1]}, i cannot invite you to the party anymore")
+    guests.pop(len(guests) - 1)
+
+print("\n")
+for i in guests:
+    print(f"Hi {i}, you are still in!!!")
+
+guests.remove("Leandro Pazienza")
+guests.remove("Albert Einstein")
+print(guests)
