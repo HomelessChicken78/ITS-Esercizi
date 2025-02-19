f1267272cd99060e36f8c1430efc339e94d20828
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 19:11:38 +0100

    Modified the exercise not to use the "for"

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-3.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-3.py
@@ -4,5 +4,14 @@ and make a list that stores several examples. Use your list to print a series of
 about these items, such as “I would like to own a Honda motorcycle.'''
 
 vehicles: list = ["Bugatti Centodieci", "Boing 747", "Tesla Model S", "Lamborghini Huracàn", "Submarine"]
+
+'''WITH FOR
 for i in vehicles:
-    print(f"I would like to own a {i},")
+    print(f"I would like to own a {i},")'''
+
+'''WITHOUT FOR'''
+print(f"I would like to own a {vehicles[0]}")
+print(f"I want to buy a {vehicles[1]}")
+print(f"I'd like to have a {vehicles[2]}")
+print(f"Why i still didn't buy a {vehicles[3]} yet?")
+print(f"Man, it would be cool to own a {vehicles[4]}!")
