cb824b5bbf4e4a548c35740becc8835d8c0bde49
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 19:13:35 +0100

    No use of the "for"

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
@@ -3,5 +3,14 @@ Greetings: Start with the list you used in Exercise 3-1, but instead of just pri
 print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.'''
 
 names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
+
+'''WITH FOR
 for i in names:
-    print(f"Hi {i}! How are you doing today?")
+    print(f"Hi {i}! How are you doing today?")'''
+
+'''WITHOUT FOR'''
+print(f"Hi {names[0]}! How are you doing today?")
+print(f"Hi {names[1]}! How are you doing today?")
+print(f"Hi {names[2]}! How are you doing today?")
+print(f"Hi {names[3]}! How are you doing today?")
+print(f"Hi {names[4]}! How are you doing today?")
