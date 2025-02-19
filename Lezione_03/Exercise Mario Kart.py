d347fa76a4c4416ed08d8f7b567725aec6d3184c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 14:44:55 +0100

    Now the "Mario Kart" exercises showcases the "match" kw  solution as well

--- Lezione_03/Exercise Mario Kart.py
+++ Lezione_03/Exercise Mario Kart.py
@@ -3,6 +3,7 @@ their race in a specific position.
 Write a Python program that takes as input the finishing position of a player, given a positive integer,
 and returns the position in cardinal form (e.g, "1st", "2nd", "3rd", "4th", "5th", etc.).'''
 
+'''Solution I - If, Elif & Else
 n: int = int(input("Write a position: "))
 if (n == 1):
     print("1st")
@@ -11,4 +12,16 @@ elif (n == 2):
 elif (n == 3):
     print("3rd")
 else:
-    print(f"{n}th")
+    print(f"{n}th")'''
+
+'''Solution II - Match'''
+n: int = int(input("Write a position: "))
+match n:
+    case 1:
+        print("1st")
+    case 2:
+        print("2nd")
+    case 3:
+        print("3rd")
+    case _:
+        print(f"{n}th")
