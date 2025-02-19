ba699c7c78e0df484d305b7e8499c54600d2e3e7
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 18:55:17 +0100

    Slight changes

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 2-5.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 2-5.py
@@ -4,4 +4,4 @@ Your output should look something like the following, including the quotation ma
 Albert Einstein once said, “A person who never made a mistake never tried anything new.”'''
 
 message: str = "Don't believe everything you read on the internet just because there's a picture with a quote next to it."
-print(f'''Abraham Lincoln once didn't say: "{message}"''')
+print(f'Abraham Lincoln once didn\'t say: \"{message}\"')
