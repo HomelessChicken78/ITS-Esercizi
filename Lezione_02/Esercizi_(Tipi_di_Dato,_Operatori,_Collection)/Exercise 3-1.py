da327fc5da028c7021f983cbdbed4b4b13d6c3a1
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-02-19 19:16:14 +0100

    Variable declaration

--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-1.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-1.py
@@ -1,7 +1,7 @@
 '''Exercise 3-1
 Names: Store the names of a few of your friends in a list called names. Print each person’s name by accessing each element in the list, one at a time.'''
 
-names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
+names: list[str] = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
 print(names[0])
 print(names[1])
 print(names[2])
--- Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
+++ Lezione_02/Esercizi_(Tipi_di_Dato,_Operatori,_Collection)/Exercise 3-2.py
@@ -2,7 +2,7 @@
 Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name,
 print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.'''
 
-names: list = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
+names: list[str] = ["Valeryia", "Rachele", "Francesca", "Giorgia", "Pingproxy"]
 
 '''WITH FOR
 for i in names:
