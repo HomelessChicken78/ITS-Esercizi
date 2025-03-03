ab9f83445e625b14f7ee93b9cb3fde762d0e153e
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-03 19:03:59 +0100

    Committing an ez match exercise

---
+++ Lezione_03/Esercizi:_Match_Statement/Exercise 3C-8.py
@@ -0,0 +1,24 @@
+'''Esercizio 3C-8. Scrivere un programma in Python che legga una frase di una riga e mostri una delle seguenti risposte:
+- "Si" -> se la frase termina con un punto interrogativo (?) ed il numero dei caratteri è pari;
+- "No" -> se la frase termina con un punto interrogativo (?) ed il numero dei caratteri è dispari;
+- "Wow!" -> se la frase termina con un punto esclamativo (!)
+- "Tu dici" seguito dalla frase inserita racchiusa tra doppi apici in tutti gli altri casi.
+
+Expected Output:
+
+frase: di essere bellissimo
+Output: Tu dici "di essere bellissimo"
+'''
+
+phrase: str = input("Inserire una frase: \n>\t")
+
+match phrase:
+    case phrase if phrase[len(phrase) - 1] == "?":
+        if len(phrase) % 2 == 0: 
+            print("Si")
+        else:
+            print("No")
+    case phrase if phrase[len(phrase) - 1] == "!":
+        print("Wow")
+    case _:
+        print(f"Tu dici \"{phrase}\"")
