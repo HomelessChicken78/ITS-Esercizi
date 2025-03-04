0a0fdee7e3195889b86abbbcec16745f0c2fd685
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-04 17:48:55 +0100

    Added some comments

--- Lezione_04/Different Sums.py
+++ Lezione_04/Different Sums.py
@@ -5,16 +5,20 @@ Sum of integers from 1 to 10 is 55
 Sum of integers from 20 to 37 is 513
 Sum of integers from 35 to 49 is 630'''
 
+#Without function look at this mess:
+#from 1 to 10 
 sum: int = 0
 for i in range(1, 11):
     sum += i
 print(sum)
 
+#from 20 to 37
 sum: int = 0
 for i in range(20, 38):
     sum += i
 print(sum)
 
+#from 35 to 49
 sum: int = 0
 for i in range(35, 50):
     sum += i
