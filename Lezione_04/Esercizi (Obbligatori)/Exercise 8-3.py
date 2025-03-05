c1b19ba904abca4680cb76ec8ec5546f479bb092
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-05 19:18:26 +0100

    modified the size to use strings instead

--- Lezione_04/Esercizi (Obbligatori)/Exercise 8-3.py
+++ Lezione_04/Esercizi (Obbligatori)/Exercise 8-3.py
@@ -2,9 +2,9 @@
 The function should print a sentence summarizing the size of the shirt and the message printed on it.
 Call the function once using positional arguments to make a shirt. Call the function a second time using keyword arguments.'''
 
-def make_shirt(size: int, text: str) -> None:
+def make_shirt(size: str, text: str) -> None:
     print(f"The size of the shirt is: {size}\nThe message written on it is \"{text}\"")
 
 make_shirt(52, "Nike")
 print("----------------------------")
-make_shirt(size=52, text="Nike")
+make_shirt(size="Medium", text="Nike")
