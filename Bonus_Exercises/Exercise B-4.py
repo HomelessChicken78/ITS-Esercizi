a25b4ada5af9e607a80f43edebae3ddca305aa05
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-10 09:14:37 +0100

    Added many comments

--- Bonus_Exercises/Exercise B-4.py
+++ Bonus_Exercises/Exercise B-4.py
@@ -4,6 +4,7 @@ Do not use sets or any built-in function. Try to do so using the function you cr
 import random
 from typing import Any
 
+#This function inverts the position of two elements in a list
 def invert_indexes(original_list: list[Any], index_1: int, index_2: int = 0) -> list[Any]:
     if index_1 < 0 or index_1 >= len(original_list) or index_2 < 0 or index_2 >= len(original_list):
         print("Error: One or both indices are out of range.")
@@ -13,22 +14,29 @@ def invert_indexes(original_list: list[Any], index_1: int, index_2: int = 0) -> 
     original_list[index_2] = tmp
     return original_list
 
+#Order the list
 def order(nums: list[int]) -> list[int]:
     i: int = 0
     j: int = 0
+    #Check a number in the list.
     while i < len(nums):
         j = i + 1
+        #Check another one and iterate over it. When the number are finished, iterate the next "i"
         while j < len(nums):
+            #If the first number is bigger than the second, invert them using the function
             if nums[i] > nums[j]:
                 nums = invert_indexes(nums, i, j)
             j += 1
         i += 1
     return nums
 
+#Output Check
+#Fill in the list
 nums: list[int] = []
 for i in range(0, random.randint(3, 7)):
     n = random.randint(-100, 100)
     nums.append(n)
 
+#Print the result
 print("Unordered list: ", nums)
 print(order(nums))
