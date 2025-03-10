ff8c249d6971a51bfd2724c1b1c2e09c173c3ae2
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-10 09:37:48 +0100

    New function exercise

--- Lezione_04/Esercizi (Obbligatori)/Exercise 8-10.py
+++ Lezione_04/Esercizi (Obbligatori)/Exercise 8-10.py
@@ -2,3 +2,22 @@
 Start with a copy of your program from Exercise 8-9.
 Write a function called send_messages() that prints each text message and moves each message to a new list called sent_messages as it’s printed.
 After calling the function, print both of your lists to make sure the messages were moved correctly.'''
+
+def send_messages(text: list[str]) -> list[str]:
+    sent_messages: list[str] = []
+    for i in text:
+        print(i)
+        sent_messages.append(i)
+    return sent_messages
+
+text: list[str] = [
+    "hello there!",
+    "I love pizza",
+    "functions are cool",
+    "ICT ITS Academy",
+    "Rainbow dash",
+    "The cake is a lie",
+    "How did we get here?"
+    ]
+sent_messages:list[str] = send_messages(text)
+print("Here are both the lists:\ntext:\t\t",text, "\nsent_messages:\t", sent_messages)
