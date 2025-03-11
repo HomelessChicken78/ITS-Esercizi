193365c3d174c408e42a8152440c6833a40c2283
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-11 12:05:04 +0100

    NBew exercise

---
+++ Bonus_Exercises/Exercise B-9.py
@@ -0,0 +1,20 @@
+'''
+Allowed functions : None
+Create a function ft_fibonacci that returns the n-th element of the Fibonacci
+sequence, the first element being at the 0 index. We’ll consider that the Fibonacci
+sequence starts like this: 0, 1, 1, 2
+Here’s how it should be prototyped :
+def ft_fibonacci(index: int) -> int
+
+Obviously, ft_fibonacci has to be recursive
+If the index is less than 0, the function should return -1.'''
+
+def ft_fibonacci(index: int) -> int:
+    if index > 1:
+       return ft_fibonacci(index-1) + ft_fibonacci(index-2)
+    elif index == 1:
+        return 1
+    elif index == 0:
+        return 0
+    
+print(ft_fibonacci(11))
