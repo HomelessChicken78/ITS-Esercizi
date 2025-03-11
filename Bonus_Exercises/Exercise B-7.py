30b3e7e99b95c970aed4234a3dcd11ec63c11e9c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-11 10:46:35 +0100

    This one gave me nostalgia

---
+++ Bonus_Exercises/Exercise B-7.py
@@ -0,0 +1,49 @@
+'''Assignment name  : fizzbuzz
+
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$> '''
+
+i: int = 1
+while i <= 100:
+    if i % 5 == 0 and i % 3 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
+    i += 1
