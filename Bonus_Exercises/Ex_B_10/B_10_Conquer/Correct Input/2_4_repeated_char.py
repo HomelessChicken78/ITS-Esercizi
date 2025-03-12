6fbc55f25820cbdf2feb36bd4cfb4af01cf2502f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-12 13:34:39 +0100

    Improvements

--- Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct Input/2_4_repeated_char.py
+++ Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct Input/2_4_repeated_char.py
@@ -1,14 +1,17 @@
 '''2.4 if there are not, check if any character is repeated'''
 
 def isrepeated(num_sys: str) -> bool:
-    duplicate: str = ""
+    duplicate: str = num_sys  #This is a duplicate of the original string which can be modified freely without messing with the while loop
     i: int = 0
-    while len(num_sys) > 0:
-        duplicate += num_sys[0]
-        num_sys = num_sys[1:]
-        print(f"{duplicate[-1]} in {num_sys}")
-        if duplicate[-1] in num_sys:
+
+    while i < len(num_sys):
+        #Update the list with the characters in the string which haven't been checked yet
+        #duplicate = duplicate[1:]  #Remove the first character from the string
+        
+        #If the number appears again in future (aka if it is present in the duplicate of the string):
+        if num_sys[i] in num_sys[i+1:]:
             return False
+        i += 1
 
 if __name__ == "__main__":
     print(isrepeated(input("Insert something:\n>\t")))
