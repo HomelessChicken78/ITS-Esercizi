38d76acaab368846f82d4737504fe56bff436d67
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-12 12:39:27 +0100

    New exercise (mostly done)

--- Lezione_04/Esercizi_Obbligatori/Ex_8_16.py
+++ Lezione_04/Esercizi_Obbligatori/Ex_8_16.py
@@ -6,4 +6,21 @@ from module_name import function_name as fn
 import module_name as mn
 from module_name import *'''
 
-import Esercizi.Lezione_04.Esercizi_Obbligatori.
+#import module_name (idk why it doers not work)
+# import Ex_8_16_function
+# describe_city()
+
+#from module_name import function_name
+# from Ex_8_16_function import describe_city
+# describe_city()
+
+# from module_name import function_name as fn
+# from Ex_8_16_function import describe_city as fn
+# describe_city()
+
+# #import module_name as mn (idk why it does not work)
+# from Ex_8_16_function as mn
+
+#from module_name import *
+# from Ex_8_16_function import *
+# describe_city()
