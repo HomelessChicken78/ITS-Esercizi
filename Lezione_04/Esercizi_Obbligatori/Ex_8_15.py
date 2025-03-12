a3d6b4f71a01233b472cc53deed59d365c6fae29
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-12 12:20:14 +0100

    Changed the naming convention for numerous files

--- Lezione_04/Esercizi_Obbligatori/Ex_8_15.py
+++ Lezione_04/Esercizi_Obbligatori/Ex_8_15.py
@@ -3,5 +3,5 @@
 8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py.
 Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.'''
 
-from Exercise_8_15_models import printing_models
+from Esercizi.Lezione_04.Esercizi_Obbligatori.Ex_8_15_models import printing_models
 printing_models()
