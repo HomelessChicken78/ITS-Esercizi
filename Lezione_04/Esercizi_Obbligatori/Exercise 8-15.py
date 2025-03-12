8b9db6bf5fc0c190f89cad7a5396c72497d80a51
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-12 12:12:44 +0100

    New exercise

--- Lezione_04/Esercizi_Obbligatori/Exercise 8-15.py
+++ Lezione_04/Esercizi_Obbligatori/Exercise 8-15.py
@@ -3,4 +3,5 @@
 8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py.
 Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.'''
 
-#from EserciziObbligatori 
+from Exercise_8_15_models import printing_models
+printing_models()
