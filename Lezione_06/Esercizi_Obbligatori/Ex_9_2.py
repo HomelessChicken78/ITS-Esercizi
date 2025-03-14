ccd2cd3a3364334a77f797ad35364f22c708ce4d
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-14 11:23:14 +0100

    Newer exercises have been added

---
+++ Lezione_06/Esercizi_Obbligatori/Ex_9_2.py
@@ -0,0 +1,12 @@
+'''9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, and call describe_restaurant() for each instance.'''
+
+from Ex_9_1 import Restaurant
+
+ITS_Bakery: Restaurant = Restaurant("ITS BAKERY", "Italian")
+ITS_Bakery.describe_restaurant()
+
+my_restaurant: Restaurant = Restaurant("We are cool", "Fast Food")
+my_restaurant.describe_restaurant()
+
+quaranta: Restaurant = Restaurant("Da quaranta", "Pizzeria")
+quaranta.describe_restaurant()
