fe6afd7778d1bf18fa76849fdc3ce00bf3227648
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-14 11:26:51 +0100

    Renames

--- Lezione_06/Esercizi_Obbligatori/Ex_9_02.py
+++ Lezione_06/Esercizi_Obbligatori/Ex_9_02.py
@@ -1,6 +1,6 @@
 '''9-2. Three Restaurants: Start with your class from Exercise 9-1. Create three different instances from the class, and call describe_restaurant() for each instance.'''
 
-from Ex_9_1 import Restaurant
+from Ex_9_01 import Restaurant
 
 ITS_Bakery: Restaurant = Restaurant("ITS BAKERY", "Italian")
 ITS_Bakery.describe_restaurant()
