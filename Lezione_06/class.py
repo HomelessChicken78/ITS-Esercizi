80559113c97838166f89b891383dce08013a3660
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-14 10:43:02 +0100

    Updated it a little bit

--- Lezione_06/class.py
+++ Lezione_06/class.py
@@ -4,8 +4,12 @@ class Person:  #NOTÈ: Camel Case is the convention here.
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    
+    def get_full_name(self) -> str:  #This is a method that defines the behaviour of an object when called.
+        return (self.first_name).title() + " " + (self.last_name).title()
 
-person_1: Person = Person("Eric", "Crow", 45)  #Yes, we could technically say that a class is a new type of data
+person_1: Person = Person("eric", "Crow", 45)  #Yes, we could technically say that a class is a new type of data
+print(person_1.get_full_name())
 
 '''Additional Information: each object has a different place in the memory and that is how two objects are dinsticted.
 That means that even if two objects have the same attributes, for python they are technically different'''
