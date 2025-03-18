b37b1d908d1d7b2d78fca8fd3d4df60f17238a50
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-18 08:45:09 +0100

    Renamed the function to follow the naming convention of the others

--- Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct_Input/n_2_1_Only_one_character.py
+++ Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct_Input/n_2_1_Only_one_character.py
@@ -1,9 +1,9 @@
 '''2.1 check if it contains only one character'''
 
-def is_long_enough(num_sys: str) -> bool:
+def islongenough(num_sys: str) -> bool:
     if len(num_sys) <= 1:
         return False
     return True
     
 if __name__ == "__main__":
-    print(is_long_enough(input("Insert something:\n>\t")))
+    print(islongenough(input("Insert something:\n>\t")))
