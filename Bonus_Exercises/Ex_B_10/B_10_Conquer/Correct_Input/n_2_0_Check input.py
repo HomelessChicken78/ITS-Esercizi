507d374907703d82e1a76893a43d169da194507d
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-18 09:07:13 +0100

    Now uses imports

--- Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct_Input/n_2_0_Check input.py
+++ Bonus_Exercises/Ex_B_10/B_10_Conquer/Correct_Input/n_2_0_Check input.py
@@ -1,22 +1,16 @@
 '''2. check if the input is correct'''
 
-from B_10_Conquer.Correct_Input.2_1_Only_one_character
+from n_2_1_Only_one_character import islongenough
+from n_2_2_hassign import has_sign
+from n_2_3_Non_alphanumeric import isvalid
+from n_2_4_repeated_char import isrepeated
 
 def check_input(num_sys: str) -> bool:
-    if len(num_sys) <= 1:
+    if islongenough(num_sys) and (not has_sign(num_sys)) and isvalid(num_sys) and (not isrepeated(num_sys)):
+        return True
+    else:
         return False
-    elif "+" in num_sys or "-" in num_sys:
-        return False
-    valid_char: list = [
-        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-        "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
-        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-        "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-        "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-    ]
-    for char in num_sys:
-        if char not in valid_char:
-            return False
+
 
 if __name__ == "__main__":
     print(check_input(input("Insert something:\n>\t")))
