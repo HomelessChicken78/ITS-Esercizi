ff5f6e3c73928f6e00839e91badc1f1b32a774e4
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-19 16:49:16 +0100

    _

---
+++ Lezione_04/Verifica_delle_conoscenze/Question_07.py
@@ -0,0 +1,42 @@
+'''
+Scrivi una funzione che verifica se in una stringa le parentesi '(' e ')' sono bilanciate, cioè per ogni parentesi che apre c'è la corrispondente parentesi che chiude.
+
+For example:
+print(check_parentheses("()()"))
+>>>True
+
+print(check_parentheses("(()))("))
+>>>False'''
+
+def check_parentheses(expression: str) -> bool:
+    remove_parenthesis: list[str] = list(expression)
+    print("".join(remove_parenthesis))
+    for char in expression:
+        if char == '(':
+            found = False
+
+            #Try to find a ")"
+            for nxt_char in remove_parenthesis:
+                if nxt_char == ')':
+                    remove_parenthesis.remove('(')
+                    remove_parenthesis.remove(')')
+                    print(f"New expression: {remove_parenthesis}")
+                    found = True
+                    break
+            
+            #If there is no ")" return false
+            if found == False:
+                return False
+
+    if remove_parenthesis:
+        print("list is not empty, returning false")
+        return False
+
+    return True
+        
+
+
+
+
+if __name__ ==  "__main__":
+    print(check_parentheses(")("))
