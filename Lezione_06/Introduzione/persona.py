052b69510e8db3ea9a136be70e8d349e4fd82001
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-20 15:46:53 +0100

    Some changes

--- Lezione_06/Introduzione/persona.py
+++ Lezione_06/Introduzione/persona.py
@@ -1,6 +1,11 @@
 class Persona:
     #Costruttore
     def __init__(self, name: str, lastname: str, age: int):
+        #self è una keyword che assume come valore l'oggetto 
         self.name = name
         self.lastname = lastname
         self.age = age
+
+    #Funzione che mi mostri in output i dati relativi ad una persone
+    def displayData(self) -> None:
+        print(f"Nome: {self.name}\nCognome: {self.lastname}\nEtà: {self.age}")
--- Lezione_06/Introduzione/persona_test.py
+++ Lezione_06/Introduzione/persona_test.py
@@ -4,6 +4,10 @@ from persona import Persona
 #Creo una persona
 cc: Persona = Persona("Cristiano", "Coccia", 21)
 
-print(cc)
+# print(cc)
 
-print(cc.name, cc.lastname, cc.age)
+# print(cc.name, cc.lastname, cc.age)
+
+#Richiamare la funzione "displayData" della classe "Persona" per mostrare in output i dati relativi alla persona 'cc'
+cc.displayData()
+
