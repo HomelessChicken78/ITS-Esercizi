330467b0cef8cf10db44063cac5de5bb5679b05a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-24 09:29:41 +0100

    Finished the exercise

--- Lezione_04/Esercizi_Opzionali/Ex_2.py
+++ Lezione_04/Esercizi_Opzionali/Ex_2.py
@@ -30,16 +30,34 @@ while end < start:
     print(f"\nWARNING: The second number must be lower than (or equal to) the second one!!! (first number was {start})\n")
     end = input_to_int("Type the second number:\n>\t", "\nWARNING: You did not type a number!!!\n")
 
-attempt: int = 0
+attempt: int = ((end-start) // 30) + 3
 win: bool = False
 correct_guess: int = randint(start, end)  #Generate the random number
-print(f"Cheat: {correct_guess}")
+#print(f"Cheat: {correct_guess}")
 
-while attempt < 3:
-    guess: int = input_to_int(f"Type your guess (You have {3 - attempt} attempt(s)):\n>\t", "\nWARNING: You did not type a number!!!\n")  #Ask the user to make a guess
+while attempt > 0:
+    #Ask the user to make a guess
+    guess: int = input_to_int(f"Type your guess (You have {attempt} attempt(s)):\n>\t", "\nWARNING: You did not type a number!!!\n")
+
+    #Force them to insert a valid number
+    while guess < start or guess > end:
+        print("\nWARNING: You are trying to guess outside of the range!!\n")
+        guess: int = input_to_int(f"Type your guess (You have {attempt} attempt(s)):\n>\t", "\nWARNING: You did not type a number!!!\n")
+
+    #Give them some hint if the guess is incorrect
     if guess > correct_guess:
-        print(f"The correct number is bigger than {guess}")
+        print(f"The correct number is smaller than {guess}\n")
     elif guess < correct_guess:
-        print(f"")
+        print(f"The correct number is bigger than {guess}\n")
+    
+    #If the guess is correct, exit the loop
     else:
-    attempt += 1
+        win = True
+        break
+    attempt -= 1
+
+#Print the outcome of the game
+if win:
+    print("Correct! You win!")
+else:
+    print(f"You lost! The number was {correct_guess}")
