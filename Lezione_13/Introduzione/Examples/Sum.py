c9c6e60edf7c126ab341a3d60906b5fb16b86908
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-03-27 13:37:52 +0100

    Finished the 'Sum.py' ; )

--- Lezione_13/Introduzione/Examples/Sum.py
+++ Lezione_13/Introduzione/Examples/Sum.py
@@ -1,8 +1,19 @@
 def recursiveSum(n: int) -> int:
-    if n <= 0:
+    if n < 0:  #User inputted a negative number: unsupported
+        print("Error")
         return 0
+    elif n == 0:  #Stop when you arrive to 0
+        return 0
+    # elif n <= 0.9999:
+    #     return 0
     else:
-        return n + sum(n-1)
-
-print(sum(5))
-print(sum(-5))
+        return int(n + recursiveSum(n-1))
+    
+s: str = "-----------------"
+print(recursiveSum(-5))
+print(s)
+print(recursiveSum(0))
+print(s)
+print(recursiveSum(5))
+# print(s)
+# print(recursiveSum(5.3468726))
