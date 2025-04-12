0218ec7369023a277622a3286cc4e0fe735d9904
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-04-12 14:50:33 +0200

    _

--- Python 1-4/Bonus_Exercises/Ex_B_10/B_10_Conquer/Conversion/n_4_2_from_decimal_to_other.py
+++ Python 1-4/Bonus_Exercises/Ex_B_10/B_10_Conquer/Conversion/n_4_2_from_decimal_to_other.py
@@ -3,7 +3,7 @@
 def base_conversion(decimal_number: int, new_base: int) -> list[int]:
     remainders: list[int] = []
 
-    #rCalculate the list of remainders. These will then be used for the conversion to the new base system
+    #Calculate the list of remainders. These will then be used for the conversion to the new base system
     while decimal_number > 0:
         remainders.append(decimal_number % new_base)
         decimal_number //= new_base  #Update the decimal number by dividing it
