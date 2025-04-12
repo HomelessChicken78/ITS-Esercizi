5435c48613d532fcfd2af6b3a5ed1d89e092487e
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-04-12 14:45:27 +0200

    _

--- Python 1-4/Lezione_03/Esercizi_su_cicli_e_condizioni/Execises 5-X/Exercise 5-5.py
+++ Python 1-4/Lezione_03/Esercizi_su_cicli_e_condizioni/Execises 5-X/Exercise 5-5.py
@@ -1,4 +1,4 @@
-'''5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+'''r5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
 
 • If the alien is green, print a message that the player earned 5 points.
 • If the alien is yellow, print a message that the player earned 10 points.
