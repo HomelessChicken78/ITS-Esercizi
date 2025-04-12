f12d33a8d6cb26818b1df70230d0789eae707ac3
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-04-12 14:47:31 +0200

    _

--- Python 1-4/Lezione_03/Esercizi_su_cicli_e_condizioni/Exercises 4-X/Exercise 4-1.py
+++ Python 1-4/Lezione_03/Esercizi_su_cicli_e_condizioni/Exercises 4-X/Exercise 4-1.py
@@ -1,5 +1,5 @@
 '''
-r4-1. Pizzas: Think of at least three kinds of your favorite pizza.
+4-1. Pizzas: Think of at least three kinds of your favorite pizza.
 Store these pizza names in a list, and then use a for loop to print the name of each pizza.
 • Modify your for loop to print a sentence using the name of the pizza, instead of printing just the name of the pizza.
 For each pizza, you should have one line of output containing a simple statement like I like pepperoni pizza.
