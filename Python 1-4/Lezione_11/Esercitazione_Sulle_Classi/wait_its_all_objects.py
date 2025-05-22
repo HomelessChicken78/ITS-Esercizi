325ffa21e28a051b29f125d31810e16fe79e767e
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-05-22 12:13:37 +0200

    _

--- Python 1-4/Lezione_11/Esercitazione_Sulle_Classi/wait_its_all_objects.py
+++ Python 1-4/Lezione_11/Esercitazione_Sulle_Classi/wait_its_all_objects.py
@@ -1,26 +1,28 @@
 # Wait it's all objects? Always has been
 
-class Classe1:
-    pass
-print(type(Classe1))
+print(type(int()), type(float()), type(bool()), type(str()), type(list()), type(frozenset()), type(set()), type(dict()), type(tuple()))
+print(type(None))
+print(type(...))
+print(type(slice(0, 4)))  # e.g. list[:4]. That [:4] is an object
+
+print(type(int), type(float), type(bool), type(str), type(list), type(frozenset), type(set), type(dict), type(tuple))
+
 def Funct1():
     pass
 print(type(Funct1))
 
 print(type(print))
 
+print(type(type))
+
 import numpy
 print(type(numpy))
+
+class Classe1:
+    pass
+print(type(Classe1))
 
 with open(__file__, "r") as f:
     print(type(f))
 
-print(type(...))
-
-print(type(None))
-
 print("<class 'Exception'>" if isinstance(ValueError(), Exception) else "")
-
-print(type(type))
-
-print(type(slice(0, 4)))  # e.g. list[:4]. That [:4] is an object
