4efe30d60e752b5eb856b9bc348ce37aaa2b0b7c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-05-23 18:42:11 +0200

    Operatori in js

---
+++ Web/Javascript/Lezione_003/operatori.js
@@ -0,0 +1,41 @@
+a = 9
+b = 4
+
+console.log()
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+console.log()
+a++
+console.log(a)
+a--
+a--
+console.log(a)
+
+console.log()
+console.log(a>b)
+console.log(a<b)
+console.log(a==8)
+console.log(a!=8)
+
+number = 1
+bool = true
+console.log()
+console.log(bool==number)
+console.log(bool===number)
+
+console.log()
+console.log(true || false)
+console.log(true || true)
+console.log(true && false)
+console.log(true && true)
+
+var x = "12";
+var y = "13";
+var z = x * y;
+console.log(z)
+var w = 12
+console.log(w + x)
