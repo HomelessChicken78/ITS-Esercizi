7ed6842cb66cc54bd1ddf03e568ba16a1f2758a8
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-05-28 19:30:25 +0200

    Pytest

---
+++ Python 1-4/Lezione_14/my_project/prova.py
@@ -0,0 +1,6 @@
+# assert 1>0
+# ssert 1<0
+
+n = 0
+assert 1 < n,\
+    "The Condition is False"
---
+++ Python 1-4/Lezione_14/my_project/tests/test_calculator.py
@@ -0,0 +1,44 @@
+from my_project.calculator import Calculator
+'''
+def test_addition():
+    calculation: Calculator = Calculator(10, 5)
+    assert calculation.addition() == 13, "The sum is wrong"
+
+def test_subtraction():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.subtraction() == 5, "The subtraction is wrong"
+
+def test_multiplication():
+    calculation: Calculator = Calculator(10, 5)
+    assert calculation.multiplication() == 50, "The multiplication is wrong"
+
+def test_division():
+    calculation: Calculator = Calculator(10,5)
+    assert calculation.division() == 2.00, "The division is wrong"
+'''
+
+
+# This allows us to create only one object
+import pytest
+@pytest.fixture
+def calculation():
+    return Calculator(10,5)
+
+def test_addition(calculation):
+    assert calculation.addition() == 13, "The sum is wrong"
+
+def test_subtraction(calculation):
+    assert calculation.subtraction() == 5, "The subtraction is wrong"
+
+def test_multiplication(calculation):
+    assert calculation.multiplication() == 50, "The multiplication is wrong"
+
+def test_division(calculation):
+    assert calculation.division() == 2.00, "The division is wrong"
+
+# Finally you can use 
+# pytest
+# To execute all tests
+
+# pytest -v
+# also allows you to have a more detailed overview
