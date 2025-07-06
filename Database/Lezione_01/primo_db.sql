13351b52d3ee76c304413c589811b246bd0bc91f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-07-06 12:33:25 +0200

    Aggiornato il database con ulteriori cose

--- Database/Lezione_01/primo_db.sql
+++ Database/Lezione_01/primo_db.sql
@@ -1,3 +1,23 @@
+create database esami;
+\c esami
+
+create domain posint as integer
+    check (value > 0);
+
+create domain posint_not_null as posint
+    check (value is not null);
+
+create domain string_not_null as varchar(255)
+    check (value is not null);
+
+create type indirizzo as (
+    via string_not_null,
+    cap char(5),
+    civico posint_not_null
+);
+
+create sequence docente_mat_seq;
+
 create table docente (
     mat integer primary key,
     nome varchar(100) not null,
@@ -18,3 +38,5 @@ create table incarico (
     foreign key (docente) references docente(mat),
     foreign key (corso) references corso(codice)
 );
+
+alter table docente alter column mat set default nextval('docente_mat_seq');
