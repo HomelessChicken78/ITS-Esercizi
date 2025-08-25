7532d6b138b4ae52b03ad37790287ecf36c0a82a
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-08-25 10:24:03 +0200

    Moved some exercises to another dir for more organization

--- Web/React/Lezione_002/prima-app/src/App.js
+++ Web/React/Lezione_002/prima-app/src/App.js
@@ -2,6 +2,13 @@ import Component1 from './Component1';
 import './App.css';
 import Clock from './Clock';
 
+
+import Persona from './Esercizi/Map/Esercizio_1';
+import Tabellina from './Esercizi/Map/Esercizio_2';
+import Stampanumeri from './Esercizi/Map/Esercizio_3';
+import BetterStampanumeri from './Esercizi/Map/Esercizio_4';
+import Biblioteca from './Esercizi/Map/Esercizio_5'
+
 // function getDate(date){
 //   return date.toLocaleDateString()+" "+ date.toLocaleTimeString()
 // }
@@ -14,11 +21,19 @@ function App() {
   let nome = "Cristiano"
   return (
     <div className="App">
+      {/* Esercizi sul map */}
+      <Persona name="Mario" cognome="Rossi"></Persona>
+      <Tabellina multiplier="2"></Tabellina>
+      <Stampanumeri></Stampanumeri>
+      <BetterStampanumeri></BetterStampanumeri>
+      <Biblioteca></Biblioteca>
+  
+
       <Component1>Roberto</Component1>
       <Component1>Cristiano</Component1>
       <Component1>Nicol</Component1>
       <Component1>Stefano</Component1>
-      
+
       <h1>Prima app con React di {nome}</h1>
       {/* <h2>
         {getDate(new Date())}
@@ -27,7 +42,7 @@ function App() {
       <Clock timezone="-6" country="USA"></Clock>
       <Clock timezone="7" country="Japan"></Clock>
       {/* <button onClick={saluta}> Ciao </button> */}
-      <button onClick={(e)=>saluta(e)}>Saluta</button>
+      <button onClick={(e) => saluta(e)}>Saluta</button>
     </div>
   );
 }
