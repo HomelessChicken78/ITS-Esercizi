d6931fec7748117d3b7df2b2fd548b6a169b9533
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-08-25 10:11:50 +0200

    _

--- Web/React/Lezione_004/app-vite/src/UserCrud.jsx
+++ Web/React/Lezione_004/app-vite/src/UserCrud.jsx
@@ -78,7 +78,7 @@
 
 import { useEffect, useState } from "react";
 
-const urlUser = "http://localhost:3001/users";
+const urlUser = "http://localhost:3238/users";
 const UserCrud = () => {
   const [users, setUsers] = useState([]);
 
@@ -103,14 +103,14 @@ const UserCrud = () => {
       <div className="container">
         <h1>USER CRUD</h1>
 
-        <div class="mb-3 row">
-          <label htmlfor="staticEmail" className="col-sm-2 col-form-label">
+        <div className="mb-3 row">
+          <label htmlFor="staticEmail" className="col-sm-2 col-form-label">
             Email
           </label>
           <div className="col-sm-10">
             <input
               type="text"
-              readonly
+              readOnly
               className="form-control-plaintext"
               id="staticEmail"
               value="email@example.com"
@@ -118,7 +118,7 @@ const UserCrud = () => {
           </div>
         </div>
         <div className="mb-3 row">
-          <label htmlfor="inputPassword" className="col-sm-2 col-form-label">
+          <label htmlFor="inputPassword" className="col-sm-2 col-form-label">
             Password
           </label>
           <div className="col-sm-10">
@@ -141,7 +141,7 @@ const UserCrud = () => {
         <div>
           {users.map((u) => {
             return (
-              <div className="row my-1 border">
+              <div className="row my-1 border" key={u.id}>
                 <div className="col-8 p-2 d-flex justify-content-start">
                   {u.name}
                 </div>
