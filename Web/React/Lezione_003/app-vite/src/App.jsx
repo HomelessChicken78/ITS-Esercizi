2636bfa3fa59d86c33a3f0ce7ad09a379a3df36f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-08-25 14:14:15 +0200

    removed unused stuff

--- Web/React/Lezione_003/app-vite/src/App.jsx
+++ Web/React/Lezione_003/app-vite/src/App.jsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import UserAlbum from './json_album/UserAlbum'
 import 'bootstrap/dist/css/bootstrap.css'
