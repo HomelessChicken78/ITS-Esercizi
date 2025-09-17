2cf0a728dfd174c74eac7c1db064e4ae9e4fa4e9
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-09-17 19:23:07 +0200

    _

--- Web/React/Lezione_003/app-vite/src/App.jsx
+++ Web/React/Lezione_003/app-vite/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { useState } from 'react';
 
 import Saluto from './Esercizi/Vacanze/Esercizio_01/Saluto';
 import CardUtente from './Esercizi/Vacanze/Esercizio_02/CardUtente';
@@ -15,9 +16,72 @@ import BlogApp from './Esercizi/Vacanze/Esercizio_10/BlogApp';
 import TodoApp from './Esercizi/Vacanze/Esercizio_11/TodoApp';
 
 function App() {
+  let [currentPage, setPage] = useState("Saluto")
+  let Pages = ["Saluto", "Card Utente", "Menu Ristorante", "Termostato",
+    "Campo Ricerca", "Messaggio Segreto", "Aggiorna Titolo",
+    "Galleria Foto", "Modulo Contatti", "Blog App", "Todo App"]
+  function RenderCondizionale() {
+    switch (currentPage) {
+      case "Saluto":
+        return (
+          <Saluto />
+        )
+      case "Card Utente":
+        return (
+          <>
+            <CardUtente nome="Cristiano" email="cristiano003@gmail.com" imgUrl="https://placehold.co/600x400" />
+            <CardUtente nome="Gianluca" email="gianl@yahoo.com" imgUrl="https://placehold.co/250x600" />
+          </>
+        )
+      case "Menu Ristorante":
+        return (
+          <MenuRistorante />
+        )
+
+      case "Termostato":
+        return (
+          <Termostato />
+        )
+
+      case "Campo Ricerca":
+        return (
+          <CampoRicerca />
+        )
+
+      case "Messaggio Segreto":
+        return (
+          <MessaggioSegreto />
+        )
+
+      case "Aggiorna Titolo":
+        return (
+          <AggiornaTitolo />
+        )
+
+      case "Galleria Foto":
+        return (
+          <GalleriaFoto />
+        )
+
+      case "Modulo Contatti":
+        return (
+          <ModuloContatti />
+        )
+
+      case "Blog App":
+        return (
+          <BlogApp />
+        )
+
+      case "Todo App":
+        return (
+          <TodoApp />
+        )
+    }
+  }
   return (
     <>
-      <div style={{ marginLeft: "10px" }}>
+      {/* <div style={{ marginLeft: "10px" }}>
         <h1>Esercizio 1</h1>
         <Saluto />
 
@@ -71,7 +135,51 @@ function App() {
 
         <h1>Esercizio 11</h1>
         <TodoApp />
-      </div>
+      </div> */}
+
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link">
+                <b>Esercizi</b>
+              </a>
+            </li>
+            {Pages.map((page, ind) => {
+              if (page == currentPage) {
+                return (
+                  <li className="nav-link">
+                    <a className=" btn btn-primary" href="#" key={ind} onClick={() => setPage(page)}>
+                      {page}
+                    </a>
+                  </li>
+                )
+              }
+              else {
+                return (
+                  <li className="nav-item">
+                    <a className="nav-link btn btn-secondary" href="#" key={ind} onClick={() => setPage(page)}>
+                      {page}
+                    </a>
+                  </li>
+                )
+              }
+            })}
+          </ul>
+        </div>
+      </nav>
+      <div>{RenderCondizionale()}</div>
     </>
   )
 }
