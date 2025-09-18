0558d3cceba2951223345563b1e973e0e3a42027
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-09-18 15:08:30 +0200

    added ";"

--- Database/Esercizi/Impiegati_e_Studenti/Query.sql
+++ Database/Esercizi/Impiegati_e_Studenti/Query.sql
@@ -3,53 +3,53 @@
 select nome
 from persona, impiegato
 where persona.cf = impiegato.persona
-order by data_nascita asc
+order by data_nascita asc;
 
 -- 2. Quali sono i nomi di tutti i progetti?
 
 select nome
-from progetto
+from progetto;
 
 -- 3. Quali sono gli stipendi dei direttori?
 
 select stipendio
 from impiegato
-where ruolo = 'Direttore'
+where ruolo = 'Direttore';
 
 -- 4. Quanti sono i progettisti?
 
 select count(*)
 from impiegato
-where ruolo = 'Progettista'
+where ruolo = 'Progettista';
 
 -- 5 Quanti sono i responsabili?
 
 select count(*)
 from impiegato, progetto
-where progetto.resp_prog = impiegato.persona
+where progetto.resp_prog = impiegato.persona;
 
 
 -- 7. Qual è lo stipendio medio dei segretari?
 
 select avg(stipendio)
 from impiegato
-where ruolo = 'Segretario'
+where ruolo = 'Segretario';
 
 -- 8. Qual è l'età della/o studente meno giovane?
 -- usare select(date_part('year',age(current_date, <DATA DI NASCITA>))) as eta FROM [...];
 
 select max((date_part('year',age(current_date, data_nascita)))) as eta
 from persona, studente
-where persona.cf = studente.persona
+where persona.cf = studente.persona;
 
 -- 9. Quanti sono i direttori che hanno assolto agli obblighi militari?
 
 select count(*)
 from impiegato, persona
-where impiegato.ruolo = 'Direttore' and persona.cf = impiegato.persona and persona.pos_uomo is not null
+where impiegato.ruolo = 'Direttore' and persona.cf = impiegato.persona and persona.pos_uomo is not null;
 
 -- 10. Quanti sono i progetti di cui è responsabile un'impiegata con almeno due figli?
 
 select count(*)
 from progetto, persona
-where progetto.resp_prog = persona.cf and persona.maternita >= 2
+where progetto.resp_prog = persona.cf and persona.maternita >= 2;
