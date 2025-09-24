f3ac4f72f3f76ac6cbc747962bd16aa80b86dbe1
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-09-24 13:54:32 +0200

    Esercizio 11 Vacanze - Todo app

--- Web/React/Lezione_003/app-vite/src/Esercizi/Vacanze/Esercizio_11/TodoApp.jsx
+++ Web/React/Lezione_003/app-vite/src/Esercizi/Vacanze/Esercizio_11/TodoApp.jsx
@@ -1,31 +1,57 @@
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react'
+import TodoForm from './TodoForm'
+import TodoList from './TodoList'
+
+const API_URl = 'http://localhost:3123/tasks'
 
 const TodoApp = () => {
-    const [tasks, setTasks] = useState([])
-    const [err, setErr] = useState(false)
-    const [load, setLoad] = useState(true)
+    const [task, setTasks] = useState([]);
+    const [load, setLoad] = useState(true);
+    const [error, setErr] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await fetch("http://localhost:3238/tasks")
-                const data = await res.json()
-                setTasks(data)
-                setLoad(false)
-            } catch {
-                setErr(true)
-                setLoad(false)
-            }
+    const fetchTasks = async () => {
+        try {
+            const res = await fetch(API_URl);
+            if (!res.ok) throw new Error("Errore nella fetch");
+
+            const data = await res.json();
+
+            setTasks(data);
+        } catch (err) {
+            setErr(err)
+        } finally {
+            setLoad(false)
         }
-        fetchData()
-    }, [])
+    };
 
-    return (
-        <>
-            {load && <div>Caricamento in corso...</div>}
-            {err && <div>Errore</div>}
-        </>
-    )
+    const deleteTask = async (id) => {
+        await fetch(API_URl + "/" + id,
+            { method: "DELETE" }
+        )
+        fetchTasks();
+    };
+
+    const toggleTask = async (id, completed) => {
+        await fetch(API_URl + "/" + id,
+        {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ completed: !completed })
+        }
+        )
+    fetchTasks();
+};
+
+useEffect(() => {
+    fetchTasks()
+}, []);
+
+return (
+    <>
+        <TodoForm />
+        <TodoList tasks={task} onDeleteTask={deleteTask} onToggleTask={toggleTask}></TodoList>
+    </>
+)
 }
 
 export default TodoApp
---
+++ Web/React/Lezione_003/app-vite/src/Esercizi/Vacanze/Esercizio_11/TodoItem.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoItem = ({ task, onDeleteTask, onToggleTask }) => {
+  return (
+    <li className="list-group-item d-flex justify-content-between">
+      <input type="checkbox" className="form-check-input me-2" checked={task.completed}
+      onChange={()=>{onToggleTask(task.id, task.completed)}}></input>
+      <span style={{textDecoration:task.completed ? "line-through" : "none"}}>{task.text}</span>
+      <button className="btn btn-danger" onClick={()=>onDeleteTask(task.id)}>Delete</button>
+    </li>
+  )
+}
+
+export default TodoItem
