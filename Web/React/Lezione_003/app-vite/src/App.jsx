ea72168f3043e3b096a2fda3178a516b6d5eefab
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-10-13 12:19:12 +0200

    Changed name of directory from "Vacanze" to "Agosto"

--- Web/React/Lezione_003/app-vite/src/App.jsx
+++ Web/React/Lezione_003/app-vite/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import Navigation from './Esercizi/Vacanze/Navigation';
+import Navigation from './Esercizi/Agosto/Navigation';
 import MainComponent from './Lezioni/MainComponent';
 import ProvaRoutes from './Lezioni/routes/ProvaRoutes';
 
