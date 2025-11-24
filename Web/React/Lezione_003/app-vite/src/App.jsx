4e81311993983ff3c0c1f3f48dd26a213a12663d
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-11-24 16:45:14 +0100

    _

--- Web/React/Lezione_003/app-vite/src/App.jsx
+++ Web/React/Lezione_003/app-vite/src/App.jsx
@@ -2,24 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import Navigation from './Esercizi/Agosto/Navigation';
-import MainComponent from './Lezioni/MainComponent';
-import ProvaRoutes from './Lezioni/routes/ProvaRoutes';
-
-import CheckboxMultiple from './Esercizi/Ottobre/CheckboxMultiple';
-import NascondiTesto from './Esercizi/Ottobre/NascondiTesto';
-import CambioColore from './Esercizi/Ottobre/CambioColore';
-import EchoInput from './Esercizi/Ottobre/EchoInput';
-import SelettorePizza from './Esercizi/Ottobre/SelettorePizza';
-
+import GestoreParcheggi from './Esercizi/PreparazioneEsami/GestoreParcheggi/GestoreParcheggi';
 
 function App() {
   return (
     <>
-      <NascondiTesto />
-      <CambioColore />
-      <EchoInput />
-      <SelettorePizza />
+        <GestoreParcheggi />
     </>
   )
 }
