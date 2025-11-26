c01f4368bff4ea3a3f37721565f966e6f4ee01a8
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-11-26 16:56:57 +0100

    Exercise 2 about arrays in Java

---
+++ Java/src/java1/arrays/Esercizi/Ex_02.java
@@ -0,0 +1,51 @@
+package java1.arrays.Esercizi;
+
+import java.util.Scanner;
+
+public class Ex_02 {
+	public static void main(String[] args) {
+		/*
+		 * Given an array A of n integers as input, create a second array B of the same
+		 * size and populate it by reversing the order of the elements of the first.
+		 * Example: if A is (10, 27, 13, 4), then I need to obtain B (4, 13, 27, 10).
+		 * After processing, print arrays A and B.
+		 */
+
+		Scanner scan = new Scanner(System.in);
+
+		int len;
+		do {
+			System.out.print("Insert the number of elements in the array -> ");
+			len = scan.nextInt();
+
+			if (len <= 0 || len > 256) {
+				System.out.println("Error: The number must be greater or equal than 0 and less than 256");
+			}
+		} while (len <= 0 || len > 256);
+		int[] A = new int[len];
+		System.out.println();
+
+		for (int i = 0; i < A.length; i++) {
+			System.out.print("Insert the " + (i + 1) + " element of the array -> ");
+			A[i] = scan.nextInt();
+		}
+
+		// Create a second array of the same size and populate it
+		int[] B = new int[len];
+
+		for (int i = (len - 1); i >= 0; i--) {
+			B[len - 1 - i] = A[i];
+		}
+
+		// result
+		System.out.print('[');
+		for (int i = 0; i < B.length; i++) {
+			if (i == B.length - 1) {
+				System.out.print(B[i]);
+			} else {
+				System.out.print(B[i] + ", ");
+			}
+		}
+		System.out.println(']');
+	}
+}
