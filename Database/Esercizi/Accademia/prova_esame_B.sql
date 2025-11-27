97ca65627e507fe0d3790344a5defacf2d5ec196
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-11-27 17:15:22 +0100

    Added exercise traces

--- Database/Esercizi/Accademia/prova_esame_B.sql
+++ Database/Esercizi/Accademia/prova_esame_B.sql
@@ -1,10 +1,14 @@
 -- QUERY 1 
+--Quali sono le persone con stipendio di al massimo 40000
+-- euro
 SELECT p.cognome
 FROM persona AS p
 WHERE p.stipendio <= 40000
 ;
 
 -- QUERY 2
+-- Quali sono i ricercatori che lavorano ad almeno un
+-- progetto e hanno uno stipendio di al massimo 40000
 SELECT DISTINCT p.id, p.nome, p.cognome
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
@@ -13,11 +17,15 @@ WHERE p.stipendio <= 40000
 ;
 
 -- QUERY 3
+-- Qual è il budget totale dei progetti nel db
 SELECT sum(pr.budget)
 FROM progetto AS pr
 ;
 
 -- QUERY 4
+-- Qual è il budget totale dei progetti a cui lavora ogni
+-- persona. Per ogni persona restituire nome, cognome e
+-- budget totale dei progetti nei quali è coinvolto
 SELECT p.nome, p.cognome, sum(pr.budget)
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
@@ -26,6 +34,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 5
+-- Qual è il numero di progetti a cui partecipa ogni
+-- professore ordinario. Per ogni professore ordinario,
+-- restituire nome, cognome, numero di progetti nei quali è
+-- coinvolto
 SELECT p.nome, p.cognome, count(pr.id)
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
@@ -35,6 +47,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 6
+-- Qual è il numero di assenze per malattia di ogni
+-- professore associato. Per ogni professore associato,
+-- restituire nume, cognome e numero di assenze per
+-- malattia
 SELECT p.nome, p.cognome, count(ass.id) num_malattie
 FROM persona AS p
     JOIN assenza AS ass ON p.id = ass.persona
@@ -43,6 +59,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 7
+-- Qual è il numero totale di ore, per ogni persona, dedicate
+-- al progetto con id ‘5’. Per ogni persona che lavora al
+-- progetto, restituire nome, cognome e numero di ore totali
+-- dedicate ad attività progettuali relative al progetto
 SELECT p.nome, p.cognome, sum(ap.oreDurata) tot_ore
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
@@ -51,6 +71,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 8
+-- Qual è il numero medio di ore delle attività progettuali
+-- svolte da ogni persona. Per ogni persona, restituire nome,
+-- cognome e numero medio di ore delle sue attività
+-- progettuali (in qualsivoglia progetto
 SELECT p.nome, p.cognome, avg(ap.oreDurata) media_ore
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
@@ -58,6 +82,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 9
+-- Qual è il numero totale di ore, per ogni persona, dedicate
+-- alla didattica. Per ogni persona che ha svolto attività
+-- didattica, restituire nome, cognome e numero di ore totali
+-- dedicate alla didattica
 SELECT p.nome, p.cognome, sum(anp.oreDurata) tot_ore
 FROM persona AS p
     JOIN AttivitaNonProgettuale anp ON p.id = anp.persona
@@ -66,6 +94,10 @@ GROUP BY p.id
 ;
 
 -- QUERY 10
+-- Quali sono le persone che hanno svolto attività nel WP
+-- di id ‘5’ del progetto con id ‘3’. Per ogni persona, restituire
+-- il numero totale di ore svolte in attività progettuali per il
+-- WP in questione
 SELECT sum(ap.oreDurata) tot_ore
 FROM persona AS p
     JOIN AttivitaProgetto AS ap ON p.id = ap.persona
