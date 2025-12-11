14becbe86251cff449786444ff08baacccb5e5dc
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-12-11 18:12:49 +0100

    Sort Impiegato in Java

---
+++ Java/src/java2/interfacce/sortImpiegati/CompareByDate.java
@@ -0,0 +1,11 @@
+package java2.interfacce.sortImpiegati;
+
+import java.util.Comparator;
+
+public class CompareByDate implements Comparator<Impiegato> {
+	@Override
+	public int compare(Impiegato o1, Impiegato o2) {
+		return o1.getDataAssunzione().compareTo(o2.getDataAssunzione());
+	}
+
+}
