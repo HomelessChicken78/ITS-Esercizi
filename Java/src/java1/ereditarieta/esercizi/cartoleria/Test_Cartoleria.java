f0c847f5609e71acfa05d30ac29811b81690a8f0
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-12-13 18:17:22 +0100

    _

--- Java/src/java1/ereditarieta/esercizi/cartoleria/Test_Cartoleria.java
+++ Java/src/java1/ereditarieta/esercizi/cartoleria/Test_Cartoleria.java
@@ -16,9 +16,9 @@ public class Test_Cartoleria {
         mag.aggiungiArticolo(penna1);
         mag.aggiungiArticolo(penna2);
         mag.aggiungiArticolo(penna3);
-        
+
         System.out.println(mag);
-        
+
         System.out.println("\nCerco un articolo di modello \"Tipo A\":");
         System.out.println(mag.ricercaPerModello("Tipo A"));
         System.out.println("Costo totale: " + mag.costoTotale());
@@ -28,19 +28,19 @@ public class Test_Cartoleria {
 
         Privato p1 = new Privato("Giacomo Coccodrillini", 10.00);
         Azienda a1 = new Azienda("I divani di david", 10.00);
-        
+
         Ordine o1 = new Ordine(1, p1);
         o1.aggiungiArticolo(gomma2);
         o1.aggiungiArticolo(penna1);
         o1.chiudiOrdine();
         System.out.println(p1);
-        
+
         Ordine o2 = new Ordine(2, a1);
         o2.aggiungiArticolo(gomma2);
         o2.aggiungiArticolo(penna1);
         o2.chiudiOrdine();
         System.out.println(a1);
-        
+
         // Ordine impossibile = new Ordine(3, a1);
         // impossibile.aggiungiArticolo(gomma1);
         // impossibile.aggiungiArticolo(gomma2);
@@ -50,9 +50,7 @@ public class Test_Cartoleria {
         // impossibile.aggiungiArticolo(penna1);
         // impossibile.chiudiOrdine();
         // ^^^ Non puoi inserire un saldo negativo
-        
-        System.out.println(a1);
-        
+
         // o2.chiudiOrdine(); // Ordine già chiuso
 	}
 }
