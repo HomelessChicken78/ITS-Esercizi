e3d7234011c120c905afb13cf1ab332fdcd4ccab
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-12-15 12:00:13 +0100

    Now inherit Exception instead of Runtime so it is checked

--- Java/src/java2/eccezioni/MyExceptions/NotFoundException.java
+++ Java/src/java2/eccezioni/MyExceptions/NotFoundException.java
@@ -1,6 +1,6 @@
 package java2.eccezioni.MyExceptions;
 
-public class NotFoundException extends RuntimeException {
+public class NotFoundException extends Exception {
 
 	public NotFoundException() {
 	}
