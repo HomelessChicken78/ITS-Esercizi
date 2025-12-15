6da5978973241fbfc5692a241487b4ed27f4a98e
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2025-12-15 12:16:10 +0100

    _

--- Java/src/java2/eccezioni/MyExceptions/Values/EmptyStringException.java
+++ Java/src/java2/eccezioni/MyExceptions/Values/EmptyStringException.java
@@ -3,28 +3,23 @@ package java2.eccezioni.MyExceptions.Values;
 public class EmptyStringException extends ValueNotAllowedException {
 
 	public EmptyStringException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmptyStringException(String message) {
 		super(message);
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmptyStringException(Throwable cause) {
 		super(cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmptyStringException(String message, Throwable cause) {
 		super(message, cause);
-		// TODO Auto-generated constructor stub
 	}
 
 	public EmptyStringException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
 		super(message, cause, enableSuppression, writableStackTrace);
-		// TODO Auto-generated constructor stub
 	}
 
 }
