c389b5008ab6142b63fdc1c7f02a63da62a49c6f
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-01-15 09:52:01 +0100

    Now the constructor throws an error. Better printing in console to see that a thread has started

--- Java/src/java3/multithreading/esercizi/bancanosync/ThreadConto.java
+++ Java/src/java3/multithreading/esercizi/bancanosync/ThreadConto.java
@@ -3,13 +3,16 @@ package java3.multithreading.esercizi.bancanosync;
 import java.util.Random;
 
 import java2.eccezioni.MyExceptions.ValidationExceptions.ValueNegativeException;
+import java2.eccezioni.MyExceptions.ValidationExceptions.ValueOutOfRangeException;
 
 public class ThreadConto extends Thread {
 	private final int numero;
 	private final Banca bank;
 
-	public ThreadConto(int numero, Banca bank) {
+	public ThreadConto(int numero, Banca bank) throws ValueOutOfRangeException {
 		super();
+		if (numero < 0 || numero > 9)
+			throw new ValueOutOfRangeException();
 		this.numero = numero;
 		this.bank = bank;
 	}
@@ -27,7 +30,9 @@ public class ThreadConto extends Thread {
 		return "" + numero;
 	}
 
+	@Override
 	public void run() {
+		System.out.println("Iniziato thread: " + Thread.currentThread().getName());
 		while (true) {
 			try {
 				Thread.sleep(200);
