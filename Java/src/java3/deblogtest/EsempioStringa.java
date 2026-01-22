480ba95f005a40ff1076f2684d155109b920d84b
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-01-22 16:27:50 +0100

    Debugger & Logger in Java

---
+++ Java/src/java3/deblogtest/EsempioStringa.java
@@ -0,0 +1,38 @@
+package java3.deblogtest;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class EsempioStringa {
+	private static final Logger logger = LogManager.getLogger(EsempioStringa.class);
+
+    public static void main(String[] args) {
+        EsempioStringa es = new EsempioStringa();
+        int oc = es.contaOccorrenze("Lorem ipsum test prova Cristiano", "r");
+        logger.info("log4j Funziona e le occorrenze sono: " + oc);
+        System.out.println(oc);
+    }
+
+    public boolean isPalindroma(String str1) {
+        String str2 = ""; // puoi aggiungere un nuovo breakpoint qui e tracciare lo stato del programma da questo punto in poi
+
+        for (int i = str1.length() - 1; i >= 0; i--) {
+            str2 += str1.charAt(i);
+        }
+
+        return str1.equals(str2);
+    }
+
+    public int contaOccorrenze(String str1, String token) {
+        int nOccorrenze = 0;
+
+        for (int i = 0; i <= str1.length() - token.length(); i++) {
+            String temp = str1.substring(i, i + token.length());
+            if (temp.equals(token)) {
+                nOccorrenze++;
+            }
+        }
+
+        return nOccorrenze;
+    }
+}
---
+++ Java/src/java3/deblogtest/Test_1.java
@@ -0,0 +1,7 @@
+package java3.deblogtest;
+
+public class Test_1 {
+	public static void main(String[] args) {
+		System.out.println(new EsempioStringa().isPalindroma("ciaoaic"));
+	}
+}
