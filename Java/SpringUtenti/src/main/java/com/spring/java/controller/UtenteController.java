fbd481f63ea90e2b412447bd03b88fc8f78e58b7
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-03 19:00:06 +0100

    Exercises

--- Java/SpringUtenti/src/main/java/com/spring/java/controller/UtenteController.java
+++ Java/SpringUtenti/src/main/java/com/spring/java/controller/UtenteController.java
@@ -1,5 +1,8 @@
 package com.spring.java.controller;
 
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,5 +25,35 @@ public class UtenteController {
 	@GetMapping(path = "/cerca/{idUtente}", produces = "application/json")
 	public Utente visualizza(@PathVariable int idUtente) {
 		return service.cercaPerId(idUtente);
+	}
+
+	@GetMapping(path = "/cancella/{idUtente}", produces = "application/json")
+	public Utente cancellaUtente(@PathVariable int idUtente) {
+		return service.eliminaUtente(idUtente);
+	}
+
+	@GetMapping(path = "/listaCompleta", produces = "application/json")
+	public List<Utente> listaCompletaUtenti() {
+		return service.listaUtenti();
+	}
+
+	@GetMapping(path = "/aggiornaEmail/{idUtente}", produces = "application/json")
+	public Utente changeEmail(@PathVariable int idUtente, String mail) {
+		return service.aggiornaEmail(idUtente, mail);
+	}
+
+	@GetMapping(path = "/numeroUtenti")
+	public int getNumeroUtenti() {
+		return service.getNumeroUtenti();
+	}
+
+	@GetMapping(path = "/listaNomi")
+	public List<String> getListaNomi() {
+		return service.getNomiUtenti();
+	}
+
+	@GetMapping(path = "/cercaNome", produces = "application/json")
+	public List<Utente> cercaUtentiPerNome(String nome) {
+		return service.cercaPerNome(nome);
 	}
 }
--- Java/SpringUtenti/src/main/java/com/spring/java/service/UtenteService.java
+++ Java/SpringUtenti/src/main/java/com/spring/java/service/UtenteService.java
@@ -1,5 +1,8 @@
 package com.spring.java.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.spring.java.dao.DAOUtenteMappa;
 import com.spring.java.entity.Utente;
 
@@ -13,5 +16,45 @@ public class UtenteService {
 
 	public Utente cercaPerId(int idUtente) {
 		return dao.selectById(idUtente);
+	}
+
+	public List<Utente> listaUtenti() {
+		return dao.selectAll();
+	}
+
+	public Utente eliminaUtente(int idUtente) {
+		return dao.delete(idUtente);
+	}
+
+	public Utente aggiornaEmail(int idUtente, String nuovaEmail) {
+		if (cercaPerId(idUtente) == null)
+			return null;
+		Utente utenteCancellato = dao.delete(idUtente);
+		utenteCancellato.setMail(nuovaEmail);
+		dao.insert(utenteCancellato);
+		return utenteCancellato;
+	}
+
+	public int getNumeroUtenti() {
+		return dao.selectAll().size();
+	}
+
+	public List<String>  getNomiUtenti() {
+		ArrayList<String> res = new ArrayList<>();
+
+		for (Utente ut : dao.selectAll())
+			res.add(ut.getNome());
+
+		return res;
+	}
+
+	public List<Utente> cercaPerNome(String nome) {
+		ArrayList<Utente> res = new ArrayList<>();
+
+		for (Utente ut : dao.selectAll())
+			if (ut.getNome().equals(nome))
+				res.add(ut);
+
+		return res;
 	}
 }
