26ea6c6543c2541027140b52609956ccc2e88d07
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-06 18:01:50 +0100

    Starting a new exercise

---
+++ Java/Java 4-5/Esercizi/RubricaTelefonica/src/main/java/com/rubrica/java/RubricaTelefonicaApplication.java
@@ -0,0 +1,13 @@
+package com.rubrica.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubricaTelefonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubricaTelefonicaApplication.class, args);
+	}
+
+}
---
+++ Java/Java 4-5/Esercizi/RubricaTelefonica/src/test/java/com/rubrica/java/RubricaTelefonicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubrica.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubricaTelefonicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
