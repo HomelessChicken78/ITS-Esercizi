541b93daf9311ecb00b08ba34cb8530c2039e59c
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-08 16:26:06 +0100

    Starting to move some lessons to a subfolder

---
+++ Java/Java 4-5/LezioniSpring/src/main/java/com/spring/lezioni/LezioniSpringApplication.java
@@ -0,0 +1,13 @@
+package com.spring.lezioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezioniSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LezioniSpringApplication.class, args);
+	}
+
+}
---
+++ Java/Java 4-5/LezioniSpring/src/test/java/com/spring/lezioni/LezioniSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.lezioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezioniSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
