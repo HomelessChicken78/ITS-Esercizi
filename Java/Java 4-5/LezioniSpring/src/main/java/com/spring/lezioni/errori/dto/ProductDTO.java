b30c6102b2b724385ce114ebdfec3c0212593e9b
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-12 12:45:12 +0100

    Errors in Spring
    Added example for Error Handling in Spring

---
+++ Java/Java 4-5/LezioniSpring/src/main/java/com/spring/lezioni/errori/dto/ProductDTO.java
@@ -0,0 +1,29 @@
+package com.spring.lezioni.errori.dto;
+
+public class ProductDTO {
+	private int prodId;
+	private String name;
+
+	public ProductDTO() {}
+
+	public ProductDTO(int prodId, String name) {
+		this.prodId = prodId;
+		this.name = name;
+	}
+
+	public int getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ Java/Java 4-5/LezioniSpring/src/main/java/com/spring/lezioni/errori/entity/Product.java
@@ -0,0 +1,29 @@
+package com.spring.lezioni.errori.entity;
+
+public class Product {
+	private int prodId;
+	private String name;
+
+	public Product() {}
+	
+	public Product(int prodId, String name) {
+		this.prodId = prodId;
+		this.name = name;
+	}
+
+	public int getProdId() {
+		return prodId;
+	}
+
+	public void setProdId(int prodId) {
+		this.prodId = prodId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
