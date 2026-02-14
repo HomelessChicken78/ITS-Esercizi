db33d29a47efbdebabfc042af71be289983d7330
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-14 15:46:17 +0100

    Error handler
    Added error handler for all of the previous exceptions

---
+++ Java/Java 4-5/Esercizi/ECommerce/src/main/java/com/spring/java/interceptors/ErrorHandler.java
@@ -0,0 +1,25 @@
+package com.spring.java.interceptors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.spring.java.dto.ErrorDTO;
+import com.spring.java.exception.ConflictException;
+import com.spring.java.exception.NotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+	@ExceptionHandler
+	public ResponseEntity<ErrorDTO> notFoundHandler(NotFoundException err404) {
+		ErrorDTO json = new ErrorDTO(404, err404.getMessage());
+		return new ResponseEntity<ErrorDTO>(json, HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<ErrorDTO> conflictHandler(ConflictException err409) {
+		ErrorDTO json = new ErrorDTO(409, err409.getMessage());
+		return new ResponseEntity<ErrorDTO>(json, HttpStatus.CONFLICT);
+	}
+}
