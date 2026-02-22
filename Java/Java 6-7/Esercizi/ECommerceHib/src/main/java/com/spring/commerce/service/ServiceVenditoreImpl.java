7087daa96080cee87b4a6bb1f288a7da5a8e22cb
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-22 15:48:35 +0100

    Removed unused import

--- Java/Java 6-7/Esercizi/ECommerceHib/src/main/java/com/spring/commerce/service/ServiceVenditoreImpl.java
+++ Java/Java 6-7/Esercizi/ECommerceHib/src/main/java/com/spring/commerce/service/ServiceVenditoreImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.spring.commerce.ECommerceHibApplication;
 import com.spring.commerce.dao.VenditoreDAO;
 import com.spring.commerce.dto.*;
 import com.spring.commerce.entity.Prodotto;
