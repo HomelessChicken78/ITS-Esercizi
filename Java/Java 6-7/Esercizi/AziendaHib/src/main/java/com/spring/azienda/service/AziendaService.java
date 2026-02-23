4b2992e2b39617d45105ffab8d522911da24e151
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-23 10:56:47 +0100

    Adding some methods

--- Java/Java 6-7/Esercizi/AziendaHib/src/main/java/com/spring/azienda/service/AziendaService.java
+++ Java/Java 6-7/Esercizi/AziendaHib/src/main/java/com/spring/azienda/service/AziendaService.java
@@ -1,5 +1,17 @@
 package com.spring.azienda.service;
 
+import java.util.Collection;
+
+import com.spring.azienda.dto.*;
+
 public interface AziendaService {
-	
+	public AziendaDTO inserisciNuova(AziendaDTO azienda);
+
+	public AziendaDTO visualizzaDati(int idAzienda);
+
+	public AziendaDatiBaseDTO visualizzaDatiBase(int idAzienda);
+
+	public Collection<AziendaDTO> visualizzaTutte();
+
+	public AziendaDatiBaseAndNumeroDipendentiDTO visualizzaDatiBaseENumeroDipendenti(int idAzienda);
 }
