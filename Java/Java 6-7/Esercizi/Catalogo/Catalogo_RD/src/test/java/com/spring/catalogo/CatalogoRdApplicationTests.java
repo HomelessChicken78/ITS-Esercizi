a956fd0870f0c44c0e3c8d7646896d65a99e5b41
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-27 10:34:25 +0100

    Added microservice Catalogo_RD

---
+++ Java/Java 6-7/Esercizi/Catalogo/Catalogo_RD/src/test/java/com/spring/catalogo/CatalogoRdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.catalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoRdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
