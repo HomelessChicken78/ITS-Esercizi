a1ffd414e04a231fa88626f51a3b0cf783852dcf
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-02-28 12:30:49 +0100

    Removed Ribbon

--- Java/Java 6-7/Esercizi/Catalogo/Catalogo_RD/src/main/java/com/spring/catalogo/CatalogoRdApplication.java
+++ Java/Java 6-7/Esercizi/Catalogo/Catalogo_RD/src/main/java/com/spring/catalogo/CatalogoRdApplication.java
@@ -2,12 +2,12 @@ package com.spring.catalogo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CatalogoRdApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CatalogoRdApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoRdApplication.class, args);
+    }
 }
--- Java/Java 6-7/Esercizi/Catalogo/Catalogo_RD/src/main/java/com/spring/catalogo/service/CatalogoWRFeign.java
+++ Java/Java 6-7/Esercizi/Catalogo/Catalogo_RD/src/main/java/com/spring/catalogo/service/CatalogoWRFeign.java
@@ -1,5 +1,6 @@
 package com.spring.catalogo.service;
 
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.spring.catalogo.dto.ProdottoDTO;
 
 @FeignClient("catalogo-WR")
+@EnableFeignClients 
 public interface CatalogoWRFeign {
 	@GetMapping(value = "/prodotti/{idProd}", produces = "application/json")
 	ProdottoDTO getProdotto(@PathVariable int idProd);
