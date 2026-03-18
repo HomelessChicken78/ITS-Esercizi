375c03059ab0a97d4b67c752bd03b37fd85b24c9
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-03-18 15:36:26 +0100

    CHORE remade the project from scratch

--- Java/Java 6-7/Esercizi/src/main/java/com/spring/catalogo/CatalogoWrStreamOutboxApplication.java
+++ Java/Java 6-7/Esercizi/src/main/java/com/spring/catalogo/CatalogoWrStreamOutboxApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatalogoWrStreamApplication {
+public class CatalogoWrStreamOutboxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CatalogoWrStreamApplication.class, args);
+		SpringApplication.run(CatalogoWrStreamOutboxApplication.class, args);
 	}
 
 }
